a1e1e55537899de8d4d3a6b5500d8f4a5f14f052
LoXsaX <119329239+LoXsaX@users.noreply.github.com>
2024-06-19 17:48:24 +0400

    Initial commit

---
+++ upload_telegram.py
@@ -0,0 +1,47 @@
+import requests
+import random
+import telegram
+import os
+from dotenv import load_dotenv
+
+
+def get_num():
+    url = "https://xkcd.com/info.0.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    all_number = response.json()["num"]
+    return all_number
+
+
+def comic_page_loading():
+    filename = "python_comics.png"
+    comic_num = random.randint(1, get_num())
+    url = f"https://xkcd.com/{comic_num}/info.0.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    comic_url = response.json()["img"]
+    response_comic = requests.get(comic_url)
+    comment = response.json()["alt"]
+    with open(filename, "wb") as file:
+        file.write(response_comic.content)
+    return comment
+
+
+def auto_loading(tg_token, chat_id, comment):
+    filename = "python_comics.png"
+    bot = telegram.Bot(token=tg_token)
+    with open(filename, "rb") as file:
+        bot.send_photo(chat_id, photo=file, caption=comment)
+    os.remove("python_comics.png")
+
+
+def main():
+    load_dotenv()
+    comment = comic_page_loading()
+    tg_token = os.environ["TG_TOKEN"]
+    chat_id = os.environ["CHAT_ID"]
+    auto_loading(tg_token, chat_id, comment)
+
+
+if __name__ == "__main__":
+    main()
